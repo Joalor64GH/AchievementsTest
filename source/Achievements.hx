36ba8c5244de2322a13d2edbd64ae7f0fa7468f1
Joalor64 <rhgarcon@gmail.com>
2024-09-30 17:36:57 -0400

    fixes + additions

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -14,47 +14,101 @@ typedef AchievementStats = {
 
 class Achievements {
     public static var achievements:Array<String> = [];
-    public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
+
+    public static var achievementsMap:Map<String, Bool> = new Map();
+    public static var achievementStatsMap:Map<String, AchievementStats> = new Map();
 
     public static function load() {
         if (FlxG.save.data.achievementsMap != null)
 			achievementsMap = FlxG.save.data.achievementsMap;
+        if (FlxG.save.data.achievementStatsMap != null)
+			achievementStatsMap = FlxG.save.data.achievementStatsMap;
 
-        var path:String = Paths.txt('achievements/achList.txt');
+        FlxG.save.flush();
+
+        var path:String = Paths.txt('achievements/achList');
         if (FileSystem.exists(path)) {
 			var listContent:String = File.getContent(path);
 			var achievementsFound:Array<String> = listContent.split('\n');
 
 			for (achievement in achievementsFound) {
+                achievements.push(achievement.trim());
 				Json.parse(File.getContent(Paths.json('achievements/' + achievement)));
                 trace("achievement " + achievement + " loaded");
 			}
 		}
     }
 
-    public static function unlock(ach:String) {
+    public static function unlock(ach:String, stats:AchievementStats, onFinish:Void->Void) {
         if (!isUnlocked(ach)) {
+            showAchievement(ach, onFinish);
+
             achievementsMap.set(ach, true);
+            achievementStatsMap.set(name, stats);
             FlxG.save.data.achievementsMap = achievementsMap;
             FlxG.save.flush();
-            trace('achievement earned: $ach!');
+
+            trace('achievement earned: $ach!\nmore info:\n $stats');
         }
     }
 
     public static function isUnlocked(ach:String):Bool {
-        return achievementsMap.exists(ach);
+        return achievementsMap.exists(ach) && achievementsMap.get(ach);
     }
 
     public static function forget(ach:String) {
         if (isUnlocked(ach)) {
             achievementsMap.remove(ach);
+            achievementStatsMap.remove(ach);
             FlxG.save.data.achievementsMap = achievementsMap;
             FlxG.save.flush();
             trace('achievement $ach removed!');
         }
     }
 
-    public function showAchievement(ach:String) {
-        // to-do: show the achievement idk
+    public function showAchievement(ach:String, onFinish:Void->Void) {
+        var coolAchieve:AchievementData = cast Json.parse(File.getContent(Paths.json('achievements/' + ach)));
+
+        var achBG:FlxSprite = new FlxSprite(60, 50).makeGraphic(420, 120, FlxColor.BLACK);
+        achBG.alpha = 0;
+        add(bg);
+
+        var achIcon:FlxSprite = new FlxSprite(achBG.x + 10, achBG.y + 10); // placeholder for now
+        achIcon.makeGraphic(Std.int(150 * (2 / 3)), 150, FlxColor.YELLOW);
+        achIcon.alpha = 0;
+        add(achIcon);
+
+        var achName:FlxText = new FlxText(achIcon.x + achIcon.width + 20, achIcon.y + 16, 280, coolAchieve.name, 16);
+		achName.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT);
+        achName.alpha = 0;
+        add(achName);
+
+		var achTxt:FlxText = new FlxText(achName.x, achName.y + 32, 280, coolAchieve.desc, 16);
+		achTxt.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT);
+        achTxt.alpha = 0;
+		add(achTxt);
+
+        FlxTween.tween(achBG, {alpha: 0.65}, 1, {ease: FlxEase.quadOut});
+        FlxTween.tween(achIcon, {alpha: 1}, 1, {ease: FlxEase.quadOut});
+        FlxTween.tween(achName, {alpha: 1}, 1, {ease: FlxEase.quadOut});
+        FlxTween.tween(achTxt, {alpha: 1}, 1, {ease: FlxEase.quadOut});
+
+        new FlxTimer().start(2.5, function(tmr:FlxTimer) {
+            FlxTween.tween(achBG, {alpha: 0}, 1, {ease: FlxEase.quadOut, onComplete: (twn:FlxTween) -> {
+                achBG.kill();
+            }});
+            FlxTween.tween(achIcon, {alpha: 0}, 1, {ease: FlxEase.quadOut, onComplete: (twn:FlxTween) -> {
+                achIcon.kill();
+            }});
+            FlxTween.tween(achName, {alpha: 0}, 1, {ease: FlxEase.quadOut, onComplete: (twn:FlxTween) -> {
+                achName.kill();
+            }});
+            FlxTween.tween(achTxt, {alpha: 0}, 1, {ease: FlxEase.quadOut, onComplete: (twn:FlxTween) -> {
+                achTxt.kill();
+            }});
+
+            if (onFinish != null)
+                onFinish();
+        });
     }
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,9 +6,12 @@ class PlayState extends FlxState
 	{
 		super.create();
 
+		add(new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE));
+
 		Achievements.load();
 
 		var text = new FlxText(0, 0, 0, "Hello World", 64);
+		text.color = FlxColor.BLACK;
 		text.screenCenter();
 		add(text);
 	}
@@ -17,9 +20,13 @@ class PlayState extends FlxState
 	{
 		super.update(elapsed);
 
-		if (FlxG.keys.justPressed.U)
-			Achievements.unlock('example');
-		else if (FlxG.keys.justPressed.F)
+		if (FlxG.keys.justPressed.U) {
+			Achievements.unlock('example', {
+				date: Date.now()
+			}, () -> {
+				trace("finished showing achievement");
+			});
+		} else if (FlxG.keys.justPressed.F)
 			Achievements.forget('example');
 	}
 }
